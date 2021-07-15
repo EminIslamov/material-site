e9e6eee31e6554a364632a6b2051a719014ee0a0
emin <Magamedi98@mail.ru>
2021-07-15 23:05:20 +0300

    Добавил карточки и стили для них

--- src/App.js
+++ src/App.js
@@ -1,45 +1,197 @@
-import {AppBar, Box, Button, Container, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {
+  AppBar,
+  Box,
+  Button,
+  Card, CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import LayerIcon from "@material-ui/icons/Layers";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow:1
+    flexGrow: 1,
   },
   menuButton: {
-    marginRight: theme.spacing(1)
+    marginRight: theme.spacing(1),
   },
   title: {
-    flexGrow: 1
+    flexGrow: 1,
   },
-}))
+  mainFeaturesPost: {
+    position: "relative",
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundOverlay: "rgba(0,0,0,.9)",
+  },
+  mainFeaturesPostContent: {
+    position: "relative",
+    padding: theme.spacing(6),
+    marginTop: theme.spacing(8),
+  },
+  cardMedia: {
+    paddingTop: '56.25%'
+  },
+  cardContent: {
+    flexGrow:1
+  },
+  cardGrid: {
+    marginTop: theme.spacing(4)
+  }
+}));
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function App() {
-  const classes = useStyles()
+  const classes = useStyles();
   return (
-    <AppBar position="fixed">
-      <Container fixed>
-        <Toolbar>
-          <IconButton
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            className={classes.menuButton}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>Menu</Typography>
-          <Box mr={3}>
-            <Button color="inherit" variant="outlined">
-              Log in
+    <>
+      <AppBar position="fixed">
+        <Container fixed>
+          <div className={classes.overlay} />
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Menu
+            </Typography>
+            <Box mr={3}>
+              <Button color="inherit" variant="outlined">
+                Log in
+              </Button>
+            </Box>
+            <Button color="secondary" variant="contained">
+              Sign up
             </Button>
-          </Box>
-          <Button color="secondary" variant="contained">
-            Sign up
-          </Button>
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <main>
+        <Paper
+          className={classes.mainFeaturesPost}
+          style={{ backgroundImage: `url(https://source.unsplash.com/random)` }}
+        >
+          <Container fixed>
+            <Grid container>
+              <Grid item md={6}>
+                <div className={classes.mainFeaturesPostContent}>
+                  <Typography
+                    component="h1"
+                    variant="h3"
+                    color="inherit"
+                    gutterBottom
+                  >
+                    Blog
+                  </Typography>
+                  <Typography component="h5" color="inherit" paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque deserunt ex illum impedit incidunt magnam molestias
+                    optio quisquam suscipit voluptatum! Lorem ipsum!
+                  </Typography>
+                  <Button variant="contained" color="secondary">
+                    Learn more
+                  </Button>
+                </div>
+              </Grid>
+            </Grid>
+          </Container>
+        </Paper>
+
+        <div className={classes.mainContent}>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Web Developer Blog
+            </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
+              deserunt ex illum impedit incidunt magnam molestias optio quisquam
+              suscipit voluptatum! Lorem ipsum!
+            </Typography>
+            <div className={classes.mainButtons}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    Start Now
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Learn More
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+          </Container>
+        </div>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image={"https://source.unsplash.com/random"}
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography>Blog post</Typography>
+                    <Typography>Just postJust postJust postJust postJust postJust post</Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      View
+                    </Button>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+
+                    <LayerIcon />
+                    <PlayCircleFilledIcon />
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </>
   );
 }
 
